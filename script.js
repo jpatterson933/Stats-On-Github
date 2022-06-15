65d5b26ce6c12397407eb13a08150f499e690042
Jeffery Patterson <jpatterson933@ucla.edu>
2022-06-14 19:35:47 -0700

    changed api token and made it a secret for authorization purposes

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 import { Octokit } from 'https://cdn.skypack.dev/@octokit/rest';
 
 const octokit = new Octokit({
-    auth: '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',
+    auth: Secret.API_Token,
     userAgent: "Jeff's Stats",
     baseUrl: 'https://api.github.com',
 })
 
-const repoActivity = await octokit.request('GET /repos/{owner}/{repo}/stats/commit_activity', {
-    owner: 'jpatterson933',
-    repo: 'resume'
-  })
+// const repoActivity = await octokit.request('GET /repos/{owner}/{repo}/stats/commit_activity', {
+//     owner: 'jpatterson933',
+//     repo: 'resume'
+// })
+//     .then((res) => {
+//         console.log(res);
+//     })
 
-console.log(octokit)
-console.log(repoActivity)
+octokit
+    .paginate("GET /repos/{owner}/{repo}/stats/commit_activity", {
+        owner: "jpatterson933",
+        repo: "resume",
+    })
+    .then((res) => {
+        // issues is an array of all issue objects. It is not wrapped in a { data, headers, status, url } object
+        // like results from `octokit.request()` or any of the endpoint methods such as `octokit.rest.issues.listForRepo()`
+
+        console.log(res)
+    });
+
+// console.log(octokit)
+// console.log(repoActivity)
