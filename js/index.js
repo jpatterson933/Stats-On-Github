e6ac2466fd0e6ab277b39000d8de77900d3397d0
Jeffery Patterson <jpatterson933@ucla.edu>
2022-07-13 16:37:11 -0700

    we have now connected the button to the chartData.json using a for loop function and breaking the loop once it matches with the correct repository. Next we are able to get the total Bytes of the project by using a getTotal function which uses the Object.entries and splits the language objects up into key value pairs. Using a total variable set to 0 intially, we then += add the Number(value) to the total and return the total that is assinged to a variable called repoByteTotal. Next, we will be using the total to get the percentages.

--- js/index.js
+++ js/index.js
@@ -7,8 +7,8 @@ const chartLoader = document.getElementById("chart-loader");
 chartLoader.style.display = "none";
 
 // repo data consists of ids, names and creation date
-console.log(repoData);
-console.log(languageData);
+// console.log(repoData);
+// console.log(languageData);
 
 function displayRepos(id, name) {
     const repoList = $("#repo-list");
@@ -16,7 +16,7 @@ function displayRepos(id, name) {
     repoList.append(button);
 };
 
-function repoNavBar () {
+function repoNavBar() {
     for (let i = 0; i < repoData.length; i++) {
         // loop through our json data and create buttons
         displayRepos(repoData[i].id, repoData[i].name)
@@ -24,3 +24,155 @@ function repoNavBar () {
 };
 // create our side bar
 repoNavBar();
+
+// our function to create a new canvas as well as all associated html elements
+const createCanvasElement = () => {
+    // grab html element
+    const chartWrapper = $("#pie-chart");
+    // we empty our html element and clear it before replacing with new one
+    chartWrapper.empty();
+    // create canvas pie chart
+    const pieChart = document.createElement("canvas");
+    // Create a id attribute
+    const id = document.createAttribute("id");
+    // Set the value of the class attribute:
+    id.value = "repo-lang-stats";
+    // set attribute to canvas element
+    pieChart.setAttributeNode(id);
+    chartWrapper.append(pieChart);
+};
+
+function grabRepoLanguageStats(buttonClicked) {
+
+    let repoName;
+
+    for (let i = 0; i < languageData.length; i++) {
+        if (languageData[i].repoName === buttonClicked) {
+            repoName = languageData[i];
+            break;
+        }
+    };
+
+    return repoName;
+};
+
+// get total bytes of language
+function getTotal(langBytes) {
+    let total = 0;
+    // use Object entires to split object into key value pairs
+    for (const [key, value] of Object.entries(langBytes)) {
+        // use console.log() to see the key value pairs
+        // console.log(`${key}: ${value}`);
+        // as it loops through key value pairs, we add values to total
+        total += Number(value);
+    }
+
+    //   console.log(total)
+    // return total
+    return total;
+
+
+}
+
+$("body").on("click", ".button-list", function (e) {
+
+    createCanvasElement();
+
+    const clickedButton = $("#clicked-button");
+
+    clickedButton.empty();
+    let $btn = $(this);
+    let clickedBtnName = $btn[0].name;
+
+    let repoStats = grabRepoLanguageStats(clickedBtnName);
+    let repoLanguages = repoStats.languageData;
+
+
+
+
+    console.log(repoStats.languageData)
+    console.log(clickedBtnName)
+
+    let repoByteTotal = getTotal(repoLanguages);
+    console.log(repoByteTotal)
+
+
+
+    // createChart(repoStats.languageData, clickedBtnName);
+
+
+
+
+})
+
+// our create chart function 
+const createChart = (list, btnName) => {
+    const screenWidth = window.screen.width;
+    // empty array to store data in the for loop
+    let data = [];
+    // empty array to store labels in the for loop below
+    let labels = [];
+    // createChart Variable: languageArray, clickedBtnRepoName
+    for (let i = 0; i < list.length; i++) {
+        console.log(list)
+        data.push(Number(list[i].totalBytes))
+        labels.push(list[i].language + " " + list[i].percentage)
+    }
+    // short hand for multi use colors
+    const neonGreen = "rgba(57, 211, 83, 1)";
+    const green = "rgba(38, 166, 65, 1)";
+    const turtleGreen = "rgba(0, 109, 50, 1)";
+    const darkGreen = "rgba(14, 68, 41, 1)";
+
+    // global style short hand
+    let globalStyle = Chart.defaults.global
+    let titleSize = 0;
+    // here we are setting chart font size and title size based off of screen width
+    if (screenWidth >= 750) {
+        globalStyle.defaultFontSize = 22;
+        titleSize = 25;
+
+    } else if (screenWidth <= 750) {
+        globalStyle.defaultFontSize = 10;
+        titleSize = 12;
+
+    }
+    // global styles for Chart
+    // globalStyle.defaultFont = `'Eczar', serif`;
+    globalStyle.defaultColor = 'white';
+    /// works - this is in the works by is essnetially a way to create a pie chart
+    const languagePieChart = new Chart("repo-lang-stats", {
+        type: "doughnut",
+        data: {
+            labels: labels,
+
+            datasets: [{
+                backgroundColor: [neonGreen, green, turtleGreen, darkGreen],
+                hoverBackgroundColor: [green, turtleGreen, darkGreen, neonGreen],
+                borderWidth: 2,
+                borderColor: [green, turtleGreen, darkGreen, neonGreen],
+                hoverBorderWidth: 6,
+                hoverBorderColor: [neonGreen, green, turtleGreen, darkGreen],
+
+                data: data
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: btnName,
+                fontColor: 'white',
+                fontSize: titleSize
+
+            },
+            legend: {
+                position: 'left',
+                labels: {
+                    fontColor: 'white'
+                }
+            }
+
+        }
+    });
+};
+
