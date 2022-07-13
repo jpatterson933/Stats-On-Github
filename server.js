648e9034866dea9f880405cfb3bc79fd1cb1c3db
Jeffery Patterson <jpatterson933@ucla.edu>
2022-07-13 13:37:02 -0700

    added the path file. Also added a funciton to quicken things on the fs. system. chartData.json and repoData.json are updated as of 7/13 - Added comments to entire server.js since it won't be touched for a long time. Will now be working on front end of app and connected the index.js to the json files that will allow me to show charts of what repos have what lanuages. Going to merget storeAPIData into main branch after this commit

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-// const express = require('express');
-// const path = require('path');
-// const app = express();
-// const PORT = 3000;
-// const key = require('./config/utils');
-// app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
-// app.listen(PORT, () => console.log(`Now listening on Port ${PORT}`))
-
+/* 
+  Jeffery W. Patterson
+  DFBW LLC
+*/
+// Backend files and Packages
 const { Octokit, App } = require("octokit");
 require('dotenv').config()
 // console.log(process.env)
 const fs = require('fs');
+const path = require('path');
 
 const octokit = new Octokit({
   auth: process.env.API_TOKEN,
@@ -32,79 +30,62 @@ class LanguagePieChartData {
   }
 };
 
-
-
-const repoGrab = async () => {
-  const repositories = await octokit.request('GET /user/repos?page=1&per_page=100', { type: 'owner' });
-  // console.log(repositories.data);
-
-
-  const repoBtnArray = [];
-
-
-  // a function to create our RepoButton classes - data is the data we grab from const repositories
-  const repoButtonCreator = (data) => {
-    for (let i = 0; i < data.length; i++) {
-      let d = data[i];
-      // as we loop through we create a new RepoButton class
-      const repoList = new RepoData(d.id, d.name, d.created_at);
-      // push the new classes of RepoButtons into our empty area const repoBtnArray = []
-      repoBtnArray.push(repoList);
-    }
-  };
-  // here we run the above function
-  repoButtonCreator(repositories.data);
-
-  // console.log(repoBtnArray[0].name, "repo name")
-
-  const languageGraphDataArray = [];
-  const gatherLanguageData = async (data) => {
-
-    for (let i = 0; i < data.length; i++) {
-      let d = data[i];
-      const languages = await octokit.request('GET /repos/{owner}/{repo}/languages', { owner: 'jpatterson933', repo: d.name });
-      let langData = languages.data;
-      console.log(langData)
-      const langDataChart = new LanguagePieChartData(d.name, langData)
-      languageGraphDataArray.push(langDataChart);
-
-    }
-
-    fs.writeFile("chartData.json", JSON.stringify(languageGraphDataArray), (err) => {
-      if (err)
-        console.log(err);
-      else {
-        console.log("File written successfully\n");
-        console.log("The written has the following contents:");
-        // console.log(fs.readFileSync("repoData.js", "utf8"));
-      }
-    });
-
-
-
-  };
-
-  gatherLanguageData(repoBtnArray)
-
-
-
-
-
-
-
-
-
-  fs.writeFile("repoData.json", JSON.stringify(repoBtnArray), (err) => {
+// funciton to write to file that we will use --- TYPE into gitbash node server.js
+function writeToFile (fileName, fileData) {
+  fs.writeFile(path.join(__dirname, "/./js/json", fileName), JSON.stringify(fileData), (err) => {
     if (err)
       console.log(err);
     else {
-      console.log("File written successfully\n");
-      console.log("The written has the following contents:");
-      // console.log(fs.readFileSync("repoData.js", "utf8"));
+      console.log("100% of the Data successfully saved in js/json Directory! \n");
     }
   });
 }
 
+// MAIN FUNCTION --- This function is what is saving all information to be used in front end file
+const repoGrab = async () => {
+  const repositories = await octokit.request('GET /user/repos?page=1&per_page=100', { type: 'owner' });
+  // Empty array for storing repo Classes
+  const repoClassArray = [];
+
+
+  // a function to create our RepoButton classes - data is the data we grab from const repositories
+  const repoClassCreator = (data) => {
+    // loops through data and creates new class instance storeing repo id, name and creation date
+    for (let i = 0; i < data.length; i++) {
+      let d = data[i];
+      // as we loop through we create a new RepoButton class
+      const repoList = new RepoData(d.id, d.name, d.created_at);
+      // push the new classes of RepoButtons into our empty area const repoClassArray = []
+      repoClassArray.push(repoList);
+    }
+  };
+  // Function that grabs repo data dn stores it into RepoData Classes
+  repoClassCreator(repositories.data);
+  // empty array to store the lanuage data to be used in the graph
+  const languageGraphDataArray = [];
+  // async function grabbing all language information on each repository
+  const gatherLanguageData = async (data) => {
+    // loops through repos and grabs language data for each specific repo and stores that in a LanguagePieChartData class instance
+    for (let i = 0; i < data.length; i++) {
+      // store our data[i] in d so we don't have to type it out a bunch of times
+      let d = data[i];
+      const languages = await octokit.request('GET /repos/{owner}/{repo}/languages', { owner: 'jpatterson933', repo: d.name });
+      // languages.data grabbed in the await octokit request stored in shorter variable name
+      let langData = languages.data;
+      // percentage of repos that have been loop through
+      let percentageTest = ((i/data.length)*100).toFixed(2) + "% Complete";
+      // print percentage to console letting user know how many repos have been gone through
+      console.log(percentageTest)
+      const langDataChart = new LanguagePieChartData(d.name, langData)
+      languageGraphDataArray.push(langDataChart);
+    }
+
+    // path.join(__dirname,"niktoResults","result.txt")
+    writeToFile("chartData.json", languageGraphDataArray);
+  };
+  // gathering data
+  gatherLanguageData(repoBtnArray)
+  // writing data to file
+  writeToFile("repoData.json", repoBtnArray);
+};
 repoGrab();
-
-
