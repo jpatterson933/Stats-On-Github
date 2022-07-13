f7418515df071b36e0b76e5c249491db4431a824
Jeffery Patterson <jpatterson933@ucla.edu>
2022-07-12 23:27:15 -0700

    I am now able to write all of each of my repositories language data onto a single file and parse through it

--- server.js
+++ server.js
@@ -12,61 +12,99 @@ require('dotenv').config()
 const fs = require('fs');
 
 const octokit = new Octokit({
-    auth: process.env.API_TOKEN,
-    userAgent: "Jeff's Stats",
-    baseUrl: 'https://api.github.com',
+  auth: process.env.API_TOKEN,
+  userAgent: "Jeff's Stats",
+  baseUrl: 'https://api.github.com',
 });
 
 class RepoData {
   constructor(id, name, created) {
-      this.id = id;
-      this.name = name;
-      this.created = created;
+    this.id = id;
+    this.name = name;
+    this.created = created;
+  }
+};
+
+class LanguagePieChartData {
+  constructor(repoName, languageData) {
+    this.repoName = repoName;
+    this.languageData = languageData;
   }
 };
 
 
 
 const repoGrab = async () => {
-    const repositories = await octokit.request('GET /user/repos?page=1&per_page=100', { type: 'owner' });
-    console.log(repositories.data);
+  const repositories = await octokit.request('GET /user/repos?page=1&per_page=100', { type: 'owner' });
+  // console.log(repositories.data);
 
 
-    const repoBtnArray = [];
-
-    
-    // a function to create our RepoButton classes - data is the data we grab from const repositories
-    const repoButtonCreator = (data) => {
-        for (let i = 0; i < data.length; i++) {
-            let d = data[i];
-            // as we loop through we create a new RepoButton class
-            const repoList = new RepoData(d.id, d.name, d.created_at);
-            // push the new classes of RepoButtons into our empty area const repoBtnArray = []
-            repoBtnArray.push(repoList);
-        }
-    };
-    // here we run the above function
-    repoButtonCreator(repositories.data);
-
-    console.log(repoBtnArray)
+  const repoBtnArray = [];
 
 
+  // a function to create our RepoButton classes - data is the data we grab from const repositories
+  const repoButtonCreator = (data) => {
+    for (let i = 0; i < data.length; i++) {
+      let d = data[i];
+      // as we loop through we create a new RepoButton class
+      const repoList = new RepoData(d.id, d.name, d.created_at);
+      // push the new classes of RepoButtons into our empty area const repoBtnArray = []
+      repoBtnArray.push(repoList);
+    }
+  };
+  // here we run the above function
+  repoButtonCreator(repositories.data);
+
+  // console.log(repoBtnArray[0].name, "repo name")
+
+  const languageGraphDataArray = [];
+  const gatherLanguageData = async (data) => {
+
+
+
+    for (let i = 0; i < data.length; i++) {
+      let d = data[i];
+      const languages = await octokit.request('GET /repos/{owner}/{repo}/languages', { owner: 'jpatterson933', repo: d.name });
+      let langData = languages.data;
+      console.log(langData)
+      const langDataChart = new LanguagePieChartData(d.name, langData)
+      languageGraphDataArray.push(langDataChart);
+
+    }
+
+    fs.writeFile("chartData.js", JSON.stringify(languageGraphDataArray), (err) => {
+      if (err)
+        console.log(err);
+      else {
+        console.log("File written successfully\n");
+        console.log("The written has the following contents:");
+        // console.log(fs.readFileSync("repoData.js", "utf8"));
+      }
+    });
+
+
+
+  };
+
+  gatherLanguageData(repoBtnArray)
 
 
 
 
 
-    
 
-    fs.writeFile("repoData.js", JSON.stringify(repoBtnArray), (err) => {
-        if (err)
-          console.log(err);
-        else {
-          console.log("File written successfully\n");
-          console.log("The written has the following contents:");
-          console.log(fs.readFileSync("repoData.js", "utf8"));
-        }
-      });
+
+
+
+  fs.writeFile("repoData.js", JSON.stringify(repoBtnArray), (err) => {
+    if (err)
+      console.log(err);
+    else {
+      console.log("File written successfully\n");
+      console.log("The written has the following contents:");
+      // console.log(fs.readFileSync("repoData.js", "utf8"));
+    }
+  });
 }
 
 repoGrab();
