71a5dc6f529a2c524c9b375eeb9ea95619c86a1a
jpatterson-cYRYJA <jpatterson933@ucla.edu>
2023-07-20 23:26:15 -0700

    added describe statement. Added second test for repoData.json properties

--- js/json/data.test.js
+++ js/json/data.test.js
@@ -4,17 +4,32 @@
 
 // import data from './chartData.json'
 
-const data = require('./chartData.json')
+const chartData = require('./chartData.json');
+const repoData = require('./repoData.json');
 
 describe("JSON data has required properties and types", () => {
-    test('JSON data has repoName and languageData properties and types', () => {
-        data.forEach(repository => {
-            // ensure we have basic term setup
-            expect(repository).toHaveProperty('repoName')
-            expect(repository.repoName).toEqual(expect.any(String));
+    test('chartData has repoName and languageData properties and types', () => {
+        chartData.forEach(repository => {
+            // check for properties
+            expect(repository).toHaveProperty('repoName');
             expect(repository).toHaveProperty('languageData');
+            // check the types of the properties
+            expect(repository.repoName).toEqual(expect.any(String));
             expect(repository.languageData).toEqual(expect.any(Object));
         });
+
     });
+
+    test("repoData has required propreties and keys", () => {
+        repoData.forEach(repository => {
+            // check for properties
+            expect(repository).toHaveProperty('id');
+            expect(repository).toHaveProperty('name');
+            expect(repository).toHaveProperty('created');
+
+
+        })
+    })
+
 
 })
